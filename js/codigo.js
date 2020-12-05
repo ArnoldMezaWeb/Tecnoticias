ba536b05b9206b11dc36c22524ac735e8ebeaabc
Arnold <arnoldmezaacosta@gmail.com>
2020-12-04 21:00:32 -0500

    azul

--- js/codigo.js
+++ js/codigo.js
@@ -1,6 +1,37 @@
 
-$("#left").click(function(){
+ 
+var consulta = $("#search-table").DataTable();
 
-  $("#left").css({"background":"green","width":"50px"});
+$("#inputBusqueda").keyup(function() {
 
+	consulta.search($(this).val()).draw();
+
+	$("table").css({
+		"height": "80vh",
+		"background": "black",
+		"opacity":"0.50",
+		"display": "block"
+
+		
+	})
+
+	if ($("#inputBusqueda").val() == ""){
+	$("table").css({
+		"height": "0",
+		"background": "none",
+		"display":"none"
+		
+		
+		
+
+	})
+
+	$("#search").hide()
+
+} else {
+	$("#search").fadeIn("fast");
+}
+})
+$('body').click(function() {
+ $("table").fadeOut("fast");
 });
